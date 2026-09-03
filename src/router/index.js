bbf360e8d717a4e3da212dfcc5822342b9b37438
AlexLi-Dev <840595660@qq.com>
2026-09-03 22:10:43 +0800

    登陆页路由done

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,31 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory,createWebHashHistory } from 'vue-router'
 import HelloWorld from '../components/HelloWorld.vue'
+// import Login from '../views/Login.vue'
+const Login = ()=>import('../views/Login.vue')
+
+const listRoutes={}
+
+const routes = [
+    {
+        path: '/',
+        name: 'Hello',
+        component: HelloWorld
+    },
+
+    //登陆页面
+    {
+        path: '/login',
+        // name: 'Login',
+        component: Login
+    }
+]
+
+
+
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes: [
-        {
-            path: '/',
-            name: 'Hello',
-            component: HelloWorld
-        }
-    ]
+    history: createWebHashHistory(),
+    routes: routes,
 })
 
 // 路由守卫
---
+++ src/views/Login.vue
@@ -0,0 +1,31 @@
+<script setup>
+</script>
+
+<template>
+ <div class="login-container">
+  <el-card class="box-card">
+    <div v-for="o in 4" :key="o" class="text item">{{ 'List item ' + o }}</div>
+  </el-card>
+  </div>
+</template>
+
+<style scoped>
+.login-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+}
+
+.item {
+  padding: 18px 0;
+}
+
+.text {
+  font-size:14px;
+}
+
+.box-card {
+  width: 480px;
+}
+</style>
